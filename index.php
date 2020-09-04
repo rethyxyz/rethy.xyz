e85de8794f27fd85ca64954b55fe274c0ea3f89d
Brody Rethy <brody@rethy.xyz>
2020-09-04 18:41:28 -0300

    changes

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 	$detect = new mobile_Detect;
 	 
 	if ( $detect->isMobile() ) {
-		header("Location: ./home_mobile.php");
+		header("Location: home_mobile.php");
 	} else {
-		header("Location: ./home.php");
+		header("Location: home.php");
 	}
 ?>
