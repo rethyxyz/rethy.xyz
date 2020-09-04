127fae8d91fcc58b842f6d25907713a22b3b1feb
Brody Rethy <brody@rethy.xyz>
2020-09-04 18:20:48 -0300

    changes

--- resume.php
+++ resume.php
@@ -1,33 +1,25 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="UTF-8" />
-		<meta name="viewport" content="width=device-width" />
-		<link rel="stylesheet" type="text/css" href="style.css">
-		<link rel="icon" href="./files/shroom0.jpg">
+		<?php include "head.php"; ?>
 		<title>Resume - Brody Rethy</title>
 	</head>
 
 	<body>
 		<div class="menu">
-            <img src="files/shroom0.jpg" height="15px" width="15px">
-			<a href="index.html">Home</a> |
-            <a href="articles.html">Articles</a> |
-            <a href="resume.html">Resume</a> |
-            <a href="downloads.html">Downloads</a> |
-            <a href="https://github.com/brodyrethy/">GitHub</a> |
-            <a href="contact_me.html">Contact Me</a>
-            <img src="files/shroom0.jpg" height="15px" width="15px">
+			<?php include "menu.php"; ?>
 		</div>
+
         <br>
+
 		<div class="main">
 			<embed src="files/resume.pdf" type="application/pdf" width="100%" height="2550px"/>
 		</div>
-        <ir>
+
+		<br>
+
         <div class="footer">
-            Last Edited: 2020-07-16
-            <br>
-            2020 (C) Brody Rethy. All rights reserved.
+			<?php include "footer.php"; ?>
         </div>
 	</body>
 </html>
