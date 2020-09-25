1b4fca0e33c249931c32ab12ebb1c479bcfdde00
Brody Rethy <brody@rethy.xyz>
2020-09-25 15:51:38 -0300

    better coming soon file

---
+++ template_pages/comingsoon.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+        <?php include "head.php"; ?>
+		<title>Coming Soon - Brody Rethy</title>
+	</head>
+
+	<body>
+        <div class="main">
+            <div class="menu">
+                <?php include "menu.php"; ?>
+            </div>
+
+            <p>Coming soon</p>
+
+            <div class="footer">
+                <?php include "footer.php"; ?>
+            </div>
+            <br>
+        </div>
+	</body>
+</html>
