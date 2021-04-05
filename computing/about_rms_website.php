cc9b8ff9fecb3939a521466a362067c53b108c2b
Brody Rethy <brodyrethy@gmail.com>
2021-04-05 18:47:13 -0300

    pushing  changes, and mod list

--- computing/about_rms_website.php
+++ computing/about_rms_website.php
@@ -1,100 +1,92 @@
 <html>
-<head>
-<meta charset="UTF-8" />
-<meta name="viewport" content="width=device-width" />
+	<head>
+		<?php include 'header.php';?>
+		<title>About Richard Stallman's Website - rethy.xyz</title>
+	</head>
 
-<link rel="stylesheet" type="text/css" href="style.css" />
-<link rel="icon" href="files/shroom0.jpg" />
+	<body>
+		<div class="main">
+			<h1>About Richard Stallman's Odd Website</h1>
+			<p><b><small>Created: 2021-02-20 | Modified: <?php $filename = basename($_SERVER['PHP_SELF']); echo date ("Y-m-d", filemtime($filename)); ?></small></b></p><hr>
 
-<title>About Richard Stallman's Website - rethy.xyz</title>
-</head>
+			<p><b>Note</b> I am not ridiculing either him or the things he shares on his personal website. I think that it's all great. Even more so, I enjoy that he shares the parts of life that some would see as odd. I'm hoping that more people act as he does. We, as people, still aren't completely genuine with others, but are moving in that direction by breaking societal norms more and more. I'm hopeful.</p>
 
-<body>
-<div class="main">
-<h1>About Richard Stallman's Odd Website</h1>
-<p><b><small>Created: 2021-02-20 | Modified: <?php $filename = basename($_SERVER['PHP_SELF']); echo date ("Y-m-d", filemtime($filename)); ?></small></b></p><hr>
+			<p>RMS (refering to Richard Stallman's alias/username) has a website full of some really interesting things. By interesting I mean odd. Not bad, just odd. They typically aren't things that normal people would be sharing, but Richard Stallman is far from normal.</p>
 
-<p><b>Note</b> I am not ridiculing either him or the things he shares on his personal website. I think that it's all great. Even more so, I enjoy that he shares the parts of life that some would see as odd. I'm hoping that more people act as he does. We, as people, still aren't completely genuine with others, but are moving in that direction by breaking societal norms more and more. I'm hopeful.</p>
+			<p>He's a renown figure for this, among other things. Incase you don't know, this is the big one:</p>
 
-<p>RMS (refering to Richard Stallman's alias/username) has a website full of some really interesting things. By interesting I mean odd. Not bad, just odd. They typically aren't things that normal people would be sharing, but Richard Stallman is far from normal.</p>
+			<blockquote>Richard Matthew Stallman, born March 16, 1953, also known by his initials, rms, is an American free software movement activist and programmer. He campaigns for software to be distributed in a manner such that its users receive the freedoms to use, study, distribute, and modify that software. Software that ensures these freedoms is termed free software. Stallman launched the GNU Project, founded the Free Software Foundation, developed the GNU Compiler Collection and GNU Emacs, and wrote the GNU General Public License.</blockquote>
 
-<p>He's a renown figure for this, among other things. Incase you don't know, this is the big one:</p>
+			<blockquote>Stallman pioneered the concept of copyleft, which uses the principles of copyright law to preserve the right to use, modify, and distribute free software, and is the main author of free software licenses which describe those terms, most notably the GNU General Public License (GPL), the most widely used free software license.</blockquote>
 
-<blockquote>Richard Matthew Stallman, born March 16, 1953, also known by his initials, rms, is an American free software movement activist and programmer. He campaigns for software to be distributed in a manner such that its users receive the freedoms to use, study, distribute, and modify that software. Software that ensures these freedoms is termed free software. Stallman launched the GNU Project, founded the Free Software Foundation, developed the GNU Compiler Collection and GNU Emacs, and wrote the GNU General Public License.</blockquote>
+			<p>Taken from <a href="https://en.wikipedia.org/wiki/Richard_Stallman">Wikipedia</a>. Now you know.</p>
 
-<blockquote>Stallman pioneered the concept of copyleft, which uses the principles of copyright law to preserve the right to use, modify, and distribute free software, and is the main author of free software licenses which describe those terms, most notably the GNU General Public License (GPL), the most widely used free software license.</blockquote>
+			<p>His website is a gem. Below I'll add some of his indexes verbatim. Please enjoy, as I did reviewing them.</p>
 
-<p>Taken from <a href="https://en.wikipedia.org/wiki/Richard_Stallman">Wikipedia</a>. Now you know.</p>
+			<section>
+			<p><a href="https://stallman.org/photos/rms-working/"><h2>"Working on my laptop"</h2></a></p>
 
-<p>His website is a gem. Below I'll add some of his indexes verbatim. Please enjoy, as I did reviewing them.</p>
+			<p>
+			<center>
+				<img src="files\rms_working_on_laptop.PNG" />
+			</center>
+			</p> <br>
 
+			<p>
+			<center>
+				<img src="files\rms_working_on_laptop_2.PNG" />
+			</center>
+			</p> <br>
 
-
-<section>
-<p><a href="https://stallman.org/photos/rms-working/"><h2>"Working on my laptop"</h2></a></p>
-
-<p>
-<center>
-	<img src="files\rms_working_on_laptop.PNG" />
-</center>
-</p> <br>
-
-<p>
-<center>
-	<img src="files\rms_working_on_laptop_2.PNG" />
-</center>
-</p> <br>
-
-<p>
-<center>
-	<img src="files\rms_working_on_laptop_3.PNG" /> <br> <br>
-</center>
-</p>
-</section>
+			<p>
+			<center>
+				<img src="files\rms_working_on_laptop_3.PNG" /> <br> <br>
+			</center>
+			</p>
+			</section>
 
 
 
-<section>
-<p><b><a href="https://stallman.org/articles/mouth.html"><h2>"My small mouth"</h2></a></b></p>
-<blockquote>When I was young, my mother used to say I had a big mouth.</blockquote>
+			<section>
+			<p><b><a href="https://stallman.org/articles/mouth.html"><h2>"My small mouth"</h2></a></b></p>
+			<blockquote>When I was young, my mother used to say I had a big mouth.</blockquote>
 
-<blockquote>Around 2003, I had a root canal operation in a molar at the back of my jaw. The dentist had trouble gaining access and said that my mouth was unusually small.</blockquote>
+			<blockquote>Around 2003, I had a root canal operation in a molar at the back of my jaw. The dentist had trouble gaining access and said that my mouth was unusually small.</blockquote>
 
-<blockquote>After it was done, I asked him to type up and sign a testimonial to this effect. After making copies, I gave the original to my mother to show her she was wrong about me.</blockquote>
+			<blockquote>After it was done, I asked him to type up and sign a testimonial to this effect. After making copies, I gave the original to my mother to show her she was wrong about me.</blockquote>
 
-<blockquote>Copyright (c) 2013 Richard Stallman Verbatim copying and redistribution of this entire page are permitted provided this notice is preserved.</blockquote>
-</section>
-
-
-
-<section>
-<p><b><a href="https://stallman.org/articles/on-hacking.html"><h2>"On Hacking"</h2></a></b></p>
-<p>He gives a really weird and mundane story about the essence of hacking, and that it's not just limited to computer usage.</p>
-</section>
+			<blockquote>Copyright (c) 2013 Richard Stallman Verbatim copying and redistribution of this entire page are permitted provided this notice is preserved.</blockquote>
+			</section>
 
 
 
-<section>
-<p><b><a href="https://stallman.org/articles/extreme.html"><h2>Extreme P*rn*graphy Law in the UK</h2></a></b></p>
-<p>I won't even copy and paste this one onto my website because of it's graphic nature and hot take. There are no images or videos, but it is textually graphic, so be aware if you're weak-minded (only joking).</p>
-<p>The controversy comes from 1st, his exclamation that none of this should be illegal if it causes no harm, and then the story that the gives, about a parrot sexually assaulting him. I won't go into it any further, </p>
-</section>
+			<section>
+			<p><b><a href="https://stallman.org/articles/on-hacking.html"><h2>"On Hacking"</h2></a></b></p>
+			<p>He gives a really weird and mundane story about the essence of hacking, and that it's not just limited to computer usage.</p>
+			</section>
 
-<hr>
 
-<p>
-See also:
-<ul>
-	<li><a href="making_vim_feel_like_emacs.php">Making Vim Feel Like Emacs</a></li>
-	<li><a href="an_efficient_vim.php">An Efficient Vim</a></li>
-</ul>
-</p>
 
-</div>
+			<section>
+			<p><b><a href="https://stallman.org/articles/extreme.html"><h2>Extreme P*rn*graphy Law in the UK</h2></a></b></p>
+			<p>I won't even copy and paste this one onto my website because of it's graphic nature and hot take. There are no images or videos, but it is textually graphic, so be aware if you're weak-minded (only joking).</p>
+			<p>The controversy comes from 1st, his exclamation that none of this should be illegal if it causes no harm, and then the story that the gives, about a parrot sexually assaulting him. I won't go into it any further, </p>
+			</section>
 
-<hr>
+			<hr>
 
-<?php include "footer.php"; ?>
+			<p>
+			See also:
+			<ul>
+				<li><a href="making_vim_feel_like_emacs.php">Making Vim Feel Like Emacs</a></li>
+				<li><a href="an_efficient_vim.php">An Efficient Vim</a></li>
+			</ul>
+			</p>
 
-</body>
+		</div>
+
+		<hr>
+
+		<?php include "footer.php"; ?>
+	</body>
 </html>
