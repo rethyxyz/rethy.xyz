ad23f9201faacd601da268edd9a2d40e53e8e5e9
Brody Rethy <brodyrethy@gmail.com>
2021-05-07 22:22:43 -0300

    commit

---
+++ index_test.php
@@ -0,0 +1,58 @@
+<html>
+<head>
+<?php include 'header.php';?>
+<title>Home - rethy.xyz</title>
+</head>
+
+<body>
+<?php include 'menu.php';?>
+
+<div class="main">
+<p><big><b>Schizo notes.</b></big></p>
+
+<p>Welcome to my website. Mostly, I use Linux, focusing on sysadmin and Bash scripting, but (unfortunately) have skills in Windows Administration as well. This is a website where I talk about computing, programming, visual media, literature, music, and more.</p>
+
+<p>
+Newest article:
+<ul>
+<li><a href="computing/the_fall_of_linux_desktops.php">The Fall of Linux Desktops</a></li>
+</ul>
+</p>
+
+<p>
+Categories:
+<ul>
+<!-- <li><a href="geography/index.php">Geography</a></li> -->
+<li><a href="computing/index.php">Computing</a></li>
+<li><a href="history/index.php">History</a></li>
+<li><a href="literature/index.php">Literature</a></li>
+<li><a href="music/index.php">Music</a></li>
+<li><a href="visual_media/index.php">Visual Media</a></li>
+</ul>
+</p>
+
+<p>
+What I'm doing now:
+<ul>
+<li><a href="my_life/2021.php">2021</a></li>
+</ul>
+</p>
+
+<hr>
+
+<p>
+Other pages about me:
+<ul>
+<li><a href="about_me.php">About Me</a></li>
+<li><a href="about_this_website.php">About This Website</a></li>
+<li><a href="downloads.php">Downloads</a></li>
+<li><a href="https://github.com/rethyxyz/">My GitHub</a></p></li>
+</ul>
+</p> <hr>
+
+<p> <h1><a href="sitemap.php">Sitemap</a></h1> </p>
+</div> <hr>
+
+<?php include 'footer.php';?>
+</body>
+<html>
