abdf2ba5d16e2f34c89174ae28854c4ddef72633
Brody Rethy <brodyrethy@gmail.com>
2021-07-02 23:28:34 -0300

    Updated downloads page.

--- downloads.php
+++ downloads.php
@@ -14,7 +14,7 @@
 <h2>Programs</h2>
 <p>
 <ul>
-	<li><a href="https://github.com/rethyxyz/RIS/archive/master.zip">RIS (rethyxyz's Installation Script)</a></li>
+	<li><a href="https://github.com/rethyxyz/RIS">RIS (rethyxyz's Installation Script)</a></li>
 	<ul>
     <li>My (semi-)modular Arch Linux-based distribution</li>
     <li>Replicates my text and vim-based tiling window manager (dwm) setup with dotfiles and all</li>
@@ -22,42 +22,43 @@
 </ul>
 
 <ul>
-	<li><a href="https://github.com/rethyxyz/Moses/archive/master.zip">moses.rat</a></li>
+	<li><a href="https://github.com/rethyxyz/Moses">moses.rat</a></li>
 	<ul>
     <li>A modified version of Moses</li>
     <li>A tiny RAT targeting Windows systems</li>
     <li>Interacts with zombies through temporary HTML pages hosted on a web server</li>
 	</ul>
 
-	<li><a href="https://github.com/rethyxyz/GodSpeaks/archive/master.zip">Godspeaks</a></li>
+	<li><a href="https://github.com/rethyxyz/GodSpeaks">Godspeaks</a></li>
 	<ul>
     <li>Python port of Terry Davis's program known by the same name</li>
 	</ul>
 </ul>
 
 <ul>
-<li><a href="https://github.com/rethyxyz/rb/archive/master.zip">rb</a></li>
+<li><a href="https://github.com/rethyxyz/rb">rb</a></li>
 	<ul>
 	<li>A Windows-like recycle bin for Linux</li>
 	</ul>
 </ul>
 
 <ul>
-<li><a href="https://github.com/rethyxyz/dwmbar/archive/master.zip">dwmbar</a></li>
+<li><a href="https://github.com/rethyxyz/dwmbar">dwmbar</a></li>
 	<ul>
 	<li>A modular status bar for dwm written in Bash</li>
+	<li>Complete with device and network interface auto-detection</li>
 	</ul>
 </ul>
 
 <ul>
-<li><a href="https://github.com/rethyxyz/dotfiles/archive/master.zip">General dotfiles for my ThinkPad E550 and Desktop</a></li>
+<li><a href="https://github.com/rethyxyz/dotfiles">General dotfiles for my ThinkPad E550 and Desktop</a></li>
 	<ul>
 	<li>My general configuration files for Debian for ThinkPad E550</li>
 	</ul>
 </ul>
 
 <ul>
-<li><a href="https://github.com/rethyxyz/Scripts/archive/master.zip">Every script I've written over the years</a></li>
+<li><a href="https://github.com/rethyxyz/Scripts">Every script I've written over the years</a></li>
 	<ul>
 	<li>Includes:</li>
 		<ul>
@@ -70,11 +71,11 @@
 </ul>
 
 <ul>
-<li><a href="https://github.com/rethyxyz/4chan_scraper/archive/master.zip">4chan scraper</a></li>
+<li><a href="https://github.com/rethyxyz/4chan_scraper">4chan scraper</a></li>
 </ul>
 
 <ul>
-<li><a href="https://github.com/rethyxyz/rethy.xyz/archive/master.zip">https://rethy.xyz source code</a></li>
+<li><a href="https://github.com/rethyxyz/rethy.xyz">https://rethy.xyz source code</a></li>
 </ul>
 </p>
 </div> <hr>
